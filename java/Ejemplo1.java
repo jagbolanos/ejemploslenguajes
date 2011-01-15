196e06634c9e1c1cee50b4893e230097517e14a9
Jorge <jagbolanos@jagbolanos-laptop.(none)>
2011-01-15 12:05:29 -0600

    Ejemplo en java y ejemplo decompilado

---
+++ java/Ejemplo1.java
@@ -0,0 +1,10 @@
+public class Ejemplo1 {
+
+	public static void main(String args[]) {
+		System.out.println("Hola Mundo");
+
+		for(int i = 0 ; i < 20 ; i ++) {
+			System.out.println("Iteración " + i);
+		}
+	}
+}
