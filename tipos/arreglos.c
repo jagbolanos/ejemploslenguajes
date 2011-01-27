efa66b1c37b768e1facc27b941c77fba889fbcb3
Jorge <jagbolanos@jagbolanos-laptop.(none)>
2011-01-27 07:18:17 -0600

    ejemplos de arreglos

---
+++ tipos/arreglos.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LENGTH 15
+#define ROWS 5
+#define COLUMNS 4
+
+typedef struct {
+	int atributo1;
+	float atributo2;
+	char atributo3;
+	short atributo4;
+} registro1;
+
+typedef struct {
+	int atributo1;
+	char atributo3;
+	float atributo2;
+	short atributo4;
+} registro2;
+
+
+int main() {
+	registro1 r1;
+
+	int arreglo1[LENGTH];
+	char arreglo2[LENGTH];
+
+	int matriz1[ROWS][COLUMNS];
+	char matriz2[ROWS][COLUMNS];
+	
+	int** matriz3 = (int**)malloc(sizeof(int) * ROWS * COLUMNS);
+
+	int i;
+	for(i = 0; i < LENGTH; i++) {
+		*(arreglo2+i) = 'a' + i;
+	}
+
+	for(i = 0; i < LENGTH; i++) {
+		printf("%c ", arreglo2[i]);
+	}
+
+
+	for(i = 0; i < LENGTH; i++) {
+		*(arreglo1+i) =  i * i;
+	}
+
+	for(i = 0; i < LENGTH; i++) {
+		printf("%d ", arreglo1[i]);
+	}
+
+	printf("\n");
+
+
+	int *p = *matriz1;
+
+	printf("matriz1: %X\n", &matriz1);
+	for (i = 0; i < ROWS * COLUMNS; i++) {
+		printf("%X ", (*matriz1)+i);
+		*((*matriz1)+i) = i;
+		if ((i+1) % COLUMNS == 0) {
+			printf("\n");
+		}
+	}
+
+	printf("\n");
+	printf("p: %X\n", &p);
+	for (i = 0; i < ROWS * COLUMNS; i++) {
+		printf("%X ", p+i);
+		if ((i+1) % COLUMNS == 0) {
+			printf("\n");
+		}
+	}
+
+	printf("\n");
+
+	int j;
+	for (i = 0; i < ROWS; i++) {
+		for (j = 0; j < COLUMNS; j++) {		
+			printf("%X ", &(matriz1[i][j]));
+			//printf("%d ", matriz1[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("matriz3: %X\n", &matriz3);
+	j = 0;
+	for (i = 0; i < ROWS * COLUMNS; i++) {
+		printf("%X ", matriz3+i);
+		matriz3[i][j] = i;
+		j++;
+		if ((i+1) % COLUMNS == 0) {
+			printf("\n");
+			j = 0;
+		}
+	}
+		
+	free(matriz3);
+	return 0;
+}
---
+++ tipos/tipos.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define LENGTH 15
+#define ROWS 5
+#define COLUMNS 4
+
+typedef struct {
+	int atributo1; //4
+	float atributo2; //4
+	char atributo3; //1
+	short atributo4; //2
+
+} registro1; //11
+
+typedef struct {
+	int atributo1;
+	char atributo3;
+	float atributo2;
+	short atributo4;
+} registro2;
+
+
+int main() {
+	registro1 r1;
+
+	int arreglo1[LENGTH];
+	char arreglo2[LENGTH];
+	int matriz1[ROWS][COLUMNS];
+	char matriz2[ROWS][COLUMNS];
+
+	printf("sizeof(char): %d\n",sizeof(char));
+	printf("sizeof(short): %d\n",sizeof(short));
+	printf("sizeof(int): %d\n",sizeof(int));
+	printf("sizeof(long): %d\n",sizeof(long));
+	printf("sizeof(long long): %d\n",sizeof(long long));
+	printf("sizeof(float): %d\n",sizeof(float));
+	printf("sizeof(double): %d\n",sizeof(double));
+
+	printf("sizeof(registro1): %d\n",sizeof(registro1));
+	printf("sizeof(registro2): %d\n",sizeof(registro2));
+	printf("sizeof(r1): %d\n",sizeof(r1));
+	printf("sizeof(arreglo1): %d\n",sizeof(arreglo1));
+	printf("sizeof(arreglo2): %d\n",sizeof(arreglo2));
+	printf("sizeof(matriz1): %d\n",sizeof(matriz1));
+	printf("sizeof(matriz2): %d\n",sizeof(matriz2));
+
+	
+	return 0;
+}
